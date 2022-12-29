319878f5d00cb53a299e50bc9733bc38e1c31de4
cjones1047 <cjmj1047@gmail.com>
2022-12-29 07:12:44 -0500

    Created .env and added .env to .gitignore; created pixela account with listed parameters; added pixela_user_token to .env

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import os
+import dotenv
+import requests
 
+dotenv.load_dotenv()
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+pixela_user_token = os.getenv("PIXELA_USER_TOKEN")
+
+user_params = {
+    "token": pixela_user_token,
+    "username": "cjones1047",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+response = requests.post(url=pixela_endpoint, json=user_params)
+print(response.text)
