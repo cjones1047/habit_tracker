54e9bf7b01d0085d69edc09db504b4e25da63cab
cjones1047 <cjmj1047@gmail.com>
2022-12-30 06:23:21 -0500

    Pixela graph created for tracking Cycling

--- main.py
+++ main.py
@@ -8,12 +8,31 @@ pixela_endpoint = "https://pixe.la/v1/users"
 
 pixela_user_token = os.getenv("PIXELA_USER_TOKEN")
 
+pixela_username = "cjones1047"
+
 user_params = {
     "token": pixela_user_token,
-    "username": "cjones1047",
+    "username": pixela_username,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
-print(response.text)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"https://pixe.la/v1/users/{pixela_username}/graphs"
+
+create_graph_headers = {
+    "X-USER-TOKEN": pixela_user_token
+}
+
+create_graph_json = {
+    "id": "cycling1",
+    "name": "Cycling",
+    "unit": "miles",
+    "type": "float",
+    "color": "sora"
+}
+
+create_graph_response = requests.post(url=graph_endpoint, headers=create_graph_headers, json=create_graph_json)
+print(create_graph_response.text)
